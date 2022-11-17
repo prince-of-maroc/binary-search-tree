d90c5e65e1c98c6d85b22a5a8272ab9e1177cb93
Rodrick <jesprikovic@gmail.com>
2022-11-17 18:17:03 -0500

    Add function to create BST

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function NodeFactory(data, left = null, right = null){
         right
     }
 }
+
+function createBalancedTree(arr){
+    if(arr.length > 0){
+        let mid = Math.floor(arr.length/2);
+        return NodeFactory(arr[mid], createBalancedTree(arr.slice(0,mid)), createBalancedTree(arr.slice(mid, arr.length-1)));
+    }
+}
