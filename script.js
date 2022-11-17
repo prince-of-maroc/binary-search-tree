da3efcb64a9ad4495ff2ac8484be32f23ccfb285
Rodrick <jesprikovic@gmail.com>
2022-11-17 18:17:23 -0500

    Add function to visualize BST in terminal

--- script.js
+++ script.js
@@ -12,3 +12,14 @@ function createBalancedTree(arr){
         return NodeFactory(arr[mid], createBalancedTree(arr.slice(0,mid)), createBalancedTree(arr.slice(mid, arr.length-1)));
     }
 }
+
+
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node.right !== null) {
+      prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+    }
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    if (node.left !== null) {
+      prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+    }
+}
